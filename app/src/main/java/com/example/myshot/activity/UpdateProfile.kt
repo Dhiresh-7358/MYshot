aa30c8a888e1d90616fbb452dc971c3c32fe7ba1
Dhiresh <dhiresh.yadav.virat@gmail.com>
2023-09-22 15:49:54 +0530

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/myshot/activity/UpdateProfile.kt
+++ app/src/main/java/com/example/myshot/activity/UpdateProfile.kt
@@ -2,14 +2,15 @@ package com.example.myshot.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.myshot.databinding.ActivityUpdateProfileBinding
+import com.example.myshot.databinding.ActivityUpdateprofileBinding
+
 
 
 class UpdateProfile : AppCompatActivity() {
-    private lateinit var binding: ActivityUpdateProfileBinding
+    private lateinit var binding: ActivityUpdateprofileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityUpdateProfileBinding.inflate(layoutInflater)
+        binding = ActivityUpdateprofileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.backButton.setOnClickListener {
